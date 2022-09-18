2ed010e81c73bd18571089e864885a0191d493e3
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2022-09-18 17:37:03 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function adicionarMusic(){
 	
 	var linkIframe = `<iframe width="670" height="640" src="https://www.youtube.com/embed/${myMusicInputId}" title="player" frameborder="0" allow="autoplay; clipboard-write;"></iframe>`;
 
-	var linkArtista = `<h1><font color="white">YouTube</font><h1><h2><font color="gray">Reproduzindo agora</font></h2>`;
+	var linkArtista = `<h1><font color="white">YouTube Radio</font><h1><h2><font color="gray">Reproduzindo agora</font></h2>`;
 	
 	var linkFinal = linkIframe + linkArtista;
 
