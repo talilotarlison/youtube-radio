e023cf12f5d887fb26666090eb5166f11f9c74f6
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2022-09-18 10:39:51 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,49 @@ function adicionarMusic(){
 
 }
 
+var listaMusic = [ "https://www.youtube.com/embed/ieWe5AW1uNY",
+	"https://www.youtube.com/embed/EL2R3vszMPQ",
+	"https://www.youtube.com/embed/qO2dR1tXGTY",
+	"https://www.youtube.com/embed/pWX4OzyGo5w",
+	"https://www.youtube.com/embed/UgfsbL-uHOA",
+	"https://www.youtube.com/embed/k1JHlNIZo74",
+	"https://www.youtube.com/embed/JKPp3Kshb7c",
+	"https://www.youtube.com/embed/sje0ZZZwJaA",
+	"https://www.youtube.com/embed/OcnrboRX0lQ",
+	"https://www.youtube.com/embed/7MhCwXstCRM"
+];
+
+var listaArtista = ["Justin Bieber - MY Word",
+"MUSE - Greatest Hits ",
+"G̲uns N Roses - Greatest Hit",
+"Coldplay - Greatest Hit",
+"Top 100 Best Classic Rock Of All Time",
+"Wesley Safadão - Atualizado",
+"Justin Bieber - Justice",
+"Natta - Atualizado",
+"Justin Bieber - Change",
+"Justin Bieber - freedom"
+];
+
+
+
+
+function musicAleatoria(){
+	var musicEscolhida = Math.floor(Math.random() * 11)
+	console.log(musicEscolhida)
+
+	var linkMusicNaTela = document.getElementById("video");
+
+	var linkIframe = `<iframe width="670" height="640" src="https://www.youtube.com/embed/${listaMusic[musicEscolhida]}" title="player" frameborder="0" allow="autoplay; clipboard-write;"></iframe>`;
+
+	var linkArtista = `<h1><font color="white"> ${listaArtista[musicEscolhida]}</font><h1><h2><font color="gray">Reproduzindo agora</font></h2>`;
+	var linkFinal = linkIframe + linkArtista;
+
+	linkMusicNaTela.innerHTML = linkFinal;
+
+
+
+}
 
 
 
